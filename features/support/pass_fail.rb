bf5e49263e8056136486134158c7bff31bd9dfe5
Dan Buckland <danbucklan@gmail.com>
2015-04-15 16:52:58 +0100

    Allow "x" or "f" to fail and "p" or Enter to pass

--- features/support/pass_fail.rb
+++ features/support/pass_fail.rb
@@ -8,8 +8,14 @@ module PassFail
       str = STDIN.getc.chr
       if str.chr == "p"
         true
-      else
+      elsif str.chr =="\r"
+        true
+      elsif str.chr == "f"
         false
+      elsif str.chr == "x"
+        false
+      else
+        # Need this to return to the start if any other key
       end
     ensure
       system("stty -raw echo icanon")
