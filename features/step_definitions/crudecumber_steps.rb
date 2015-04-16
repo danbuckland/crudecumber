8726fa653f2092317b112357f598bb2d8afea4ee
Dan Buckland <danbucklan@gmail.com>
2015-04-16 10:34:01 +0100

    Fix bug where terminal is not reset on quitting during test run

--- features/step_definitions/crudecumber_steps.rb
+++ features/step_definitions/crudecumber_steps.rb
@@ -1,4 +1,5 @@
 Then(/^.*$/) do
+  Cucumber.trap_interrupt
   key = capture_key
   fail unless pass?(key)
 end
--- features/support/pass_fail.rb
+++ features/support/pass_fail.rb
@@ -3,32 +3,34 @@ require 'io/console'
 module PassFail
 
   def capture_key
-    str = "v"
+    str = ""
     system("stty raw -echo isig")
-    until ["p", "\r", "f", "x"].include? str do
+    until ["p", "\r", "f", "x"].include? str.downcase do
       str = STDIN.getc.chr
     end
-    str
+    system("stty -raw echo")
+    str.downcase
   end
 
   def pass?(a)
-    case
-      when a == "p"
-        true
-      when a =="\r"
-        true
-      when a == "f"
-        false
-      when a == "x"
-        false
-      else
-      end
-    ensure
+      {
+        p: true,
+        :"\r" => true,
+        x: false,
+        f: false
+      }[a.to_sym]
+  end
+
+  def Cucumber.trap_interrupt
+
+    trap('INT') do
+      exit!(1) if Cucumber.wants_to_quit
+      Cucumber.wants_to_quit = true
       system("stty -raw echo")
+      STDERR.puts "\nPress 'Ctrl + C' again to end your test run"
+    end
   end
 
 end
 
 World(PassFail)
-
-# TODO fix bug where using ctrl+c leaves the terminal without echo and in raw
