8d2d7f39696f72ee7dc2c5a770d4f6fdd8052f5a
Dan Buckland <danbucklan@gmail.com>
2015-04-23 11:30:04 +0100

    Add press "s" to skip scenario

--- features/step_definitions/crudecumber_steps.rb
+++ features/step_definitions/crudecumber_steps.rb
@@ -1,9 +1,13 @@
 Then(/^.*$/) do
   Cucumber.trap_interrupt
   key = capture_key
-  unless pass?(key)
-    print "\nDescribe the problem:\n"
-    puts "Notes: " + STDIN.gets.chomp
-    fail
+  unless skipped?(key)
+    unless pass?(key)
+      print "\nDescribe the problem:\n"
+      puts "Notes: " + STDIN.gets.chomp
+      fail
+    end
+  else
+    pending("Step skipped")
   end
 end
--- features/support/pass_fail.rb
+++ features/support/pass_fail.rb
@@ -5,11 +5,21 @@ module PassFail
   def capture_key
     str = ""
     system("stty raw -echo isig")
-    until ["p", "\r", "f", "x"].include? str.downcase do
+    until ["p", "\r", "f", "x", "s"].include? str.downcase do
       str = STDIN.getc.chr
     end
     system("stty -raw echo")
     str.downcase
+  end
+
+  def skipped?(a)
+    {
+      s: true,
+      p: false,
+      :"\r" => false,
+      x: false,
+      f: false
+    }[a.to_sym]
   end
 
   def pass?(a)
