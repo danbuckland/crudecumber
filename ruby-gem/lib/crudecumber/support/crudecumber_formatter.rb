13047ee2de6d9063060bf25c9541b8b21796a832
Dan Buckland <dan.buckland@chelsea-apps.com>
2017-08-21 16:45:54 +0100

    Add some dubious looking code to render passed, failed skipped and pending multiline steps

--- ruby-gem/lib/crudecumber/support/crudecumber_formatter.rb
+++ ruby-gem/lib/crudecumber/support/crudecumber_formatter.rb
@@ -18,9 +18,29 @@ module Crudecumber
     end
 
     def before_step_result(*args)
-      unless args[2].nil?
-        rows = args[2].raw.length
-        @io.printf "\033[#{rows + 1}A"
+      table_rows = args[2].nil? ? 0 : args[2].raw.length
+      
+      if !args[2].nil? # If there is a table
+        case args[3]
+        when :failed
+          @io.printf "\033[#{table_rows + 3}A"
+        when :pending
+          sleep 4
+          @io.printf "\033[#{table_rows + 1}A"
+        when :passed
+          @io.printf "\033[#{table_rows + 1}A"
+        when :skipped
+        end
+      else
+        case args[3]
+        when :failed
+          @io.printf "\033[#{2}A"
+        when :pending
+          #@io.printf "\033[#{table_rows + 1}A"
+        when :passed
+          #@io.printf "\033[#{table_rows + 1}A"
+        when :skipped
+        end
       end
       @io.printf "\r\033[K"
       super
